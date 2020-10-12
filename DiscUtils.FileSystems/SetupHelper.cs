bfdb1564b495d5a456e81c7f542afd69a499ec3e
Ciastex <Ciastex@users.noreply.github.com>
2020-10-12 12:33:21 +0200

    remove all stuff i don't need

---
+++ DiscUtils.FileSystems/SetupHelper.cs
@@ -0,0 +1,13 @@
+﻿using DiscUtils.CoreCompat;
+using DiscUtils.Ntfs;
+
+namespace DiscUtils.FileSystems
+{
+    public static class SetupHelper
+    {
+        public static void SetupFileSystems()
+        {
+            Setup.SetupHelper.RegisterAssembly(ReflectionHelper.GetAssembly(typeof(NtfsFileSystem)));
+        }
+    }
+}
